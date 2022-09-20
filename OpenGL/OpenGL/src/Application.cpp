e5b4403dd049e5436ee9aec9b0c73279ad3f4814
i-am-Els <emmanomax33@gmail.com>
2022-09-20 17:45:38 +0100

    Vertex buffers and drawing a triangle with modern opengl

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -29,18 +29,32 @@ int main(void)
     /* Get the OpenGl version to screen */
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float position[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /*Legacy openGl*/
+        /*glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
 
-        glEnd();
+        glEnd();*/
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
